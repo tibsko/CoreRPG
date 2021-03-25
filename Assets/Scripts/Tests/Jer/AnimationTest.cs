66f239585228d8f74192bab6686526e6d166a3b0
Jer D <dambraij@gmail.com>
2021-03-25 19:21:24 +0100

    Import animations

---
+++ Assets/Scripts/Tests/Jer/AnimationTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+
+    public Animator animator;
+    private bool isShooting = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("IsShooting="+ isShooting);
+            isShooting = !isShooting;
+            animator.SetTrigger("Shoot");
+        }
+    }
+}
