f345ea1440bba3e7eab8a2d2fe456404bab83ad7
Jer D <dambraij@gmail.com>
2021-04-23 00:58:32 +0200

    Small change name

--- Assets/Scripts/Utils/DelayExtensions.cs
+++ Assets/Scripts/Utils/DelayExtensions.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public static class DelayExtensions {
 
     public static void InvokeDelay(this MonoBehaviour mono, Action method, float delay) {
-        mono.StartCoroutine(InvokeCoroutine(method, delay));
+        mono.StartCoroutine(InvokeDelayCoroutine(method, delay));
     }
 
-    private static IEnumerator InvokeCoroutine(Action method, float delay) {
+    private static IEnumerator InvokeDelayCoroutine(Action method, float delay) {
         yield return new WaitForSeconds(delay);
         method();
     }
