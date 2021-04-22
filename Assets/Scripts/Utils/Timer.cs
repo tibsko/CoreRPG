3fc1035e68d6711b43c832342114ce8363c34aa6
Jer D <dambraij@gmail.com>
2021-04-23 00:57:46 +0200

    add timer functionnalities

--- Assets/Scripts/Utils/Timer.cs
+++ Assets/Scripts/Utils/Timer.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Timer {
     public float Time { get; private set; }
-    public float TargetTime { get; private set; }
-
-
+    public float TargetTime { get; set; }
     public bool Done { get => Time >= TargetTime; }
 
     public Timer(float targetTimer) {
@@ -18,4 +16,26 @@ public class Timer {
         Time += deltaTime;
     }
 
+    public void Reset() {
+        Time = 0;
+    }
+}
+
+public class InvTimer {
+    public float Time { get; private set; }
+    public float StartTime { get; private set; }
+    public bool Done { get => Time <= 0; }
+
+    public InvTimer(float startTimer) {
+        StartTime = startTimer;
+        Time = StartTime;
+    }
+
+    public void Tick(float deltaTime) {
+        Time -= deltaTime;
+    }
+
+    public void Reset() {
+        Time = StartTime;
+    }
 }
