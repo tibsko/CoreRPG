f1f6a88232f73c901d9f8433ba4276861723c590
Jer D <dambraij@gmail.com>
2021-05-15 11:43:37 +0200

    Pull & WorldRot

---
+++ Assets/WorldRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRotation : MonoBehaviour
+{
+    void Start()
+    {
+        transform.Rotate(new Vector3(0, 45,0));
+    }
+}
