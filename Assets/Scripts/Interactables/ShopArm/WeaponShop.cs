6357a1e9a0c72dfacbad7edd0ea2ff2b0652df00
Jer D <dambraij@gmail.com>
2021-05-16 19:20:29 +0200

    Remove unused files

---
+++ Assets/Scripts/Interactables/ShopArm/WeaponShop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShop : MonoBehaviour {
+
+    [SerializeField] Weapon weapon;
+    [SerializeField] int weaponPrice;
+
+    public void Buy(GameObject player) {
+        PlayerWeapons playerWeapons = player.GetComponent<PlayerWeapons>();
+        PlayerMoney playerMoney = player.GetComponent<PlayerMoney>();
+        if (playerWeapons && playerMoney && playerMoney.currentMoney >= weaponPrice) {
+            playerWeapons.AddWeapon(weapon);
+            playerMoney.LooseMoney(weaponPrice);
+        }
+    }
+}
