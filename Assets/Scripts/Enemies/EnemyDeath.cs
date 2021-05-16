8a9365e9a588de34132889c8152dbd547586decb
Jer D <dambraij@gmail.com>
2021-05-16 19:42:06 +0200

    Add EnemyDeath script

---
+++ Assets/Scripts/Enemies/EnemyDeath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDeath : MonoBehaviour
+{
+    [SerializeField] float destroyTimer;
+
+    public void EnableRagdoll() {
+        gameObject.layer = 0;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<ZombieController>().enabled = false;
+        GetComponent<ZombieAttack>().enabled = false;
+        GetComponentInChildren<Animator>().enabled = false;
+        GetComponentInChildren<Canvas>().enabled = false;
+        SetKinematic(false);
+        GetComponent<Rigidbody>().isKinematic = true;
+        Destroy(gameObject, destroyTimer);
+        this.enabled = false;
+    }
+
+    private void SetKinematic(bool newValue) {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies) {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = newValue;
+        }
+    }
+}
--- Assets/Scripts/Stats/EnemyHealth.cs
+++ Assets/Scripts/Stats/EnemyHealth.cs
@@ -5,27 +5,5 @@ using UnityEngine.AI;
 
 public class EnemyHealth : GenericHealth {
 
-    [SerializeField] float timer;
-
-    public void Die() {
-        gameObject.layer = 0;
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-        GetComponent<NavMeshAgent>().enabled = false;
-        GetComponent<ZombieController>().enabled = false;
-        GetComponent<ZombieAttack>().enabled = false;
-        GetComponentInChildren<Animator>().enabled = false;
-        GetComponentInChildren<Canvas>().enabled = false;
-        SetKinematic(false);
-        GetComponent<Rigidbody>().isKinematic = true;
-        Destroy(gameObject,timer);
-        this.enabled = false;
-    }
-
-    private void SetKinematic(bool newValue) {
-        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
-        foreach (Rigidbody rb in bodies) {
-            rb.velocity = Vector3.zero;
-            rb.isKinematic = newValue;
-        }
-    }
+   
 }
