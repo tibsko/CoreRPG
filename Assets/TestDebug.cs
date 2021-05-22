7817d1f9397c814cc57e0f1e0a975b6868493506
Jer D <dambraij@gmail.com>
2021-05-22 23:08:07 +0200

    Create TestDebug.cs

---
+++ Assets/TestDebug.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDebug : MonoBehaviour
+{
+    void Update()
+    {
+        Debug2.Log("Collision !", "Physics");
+        Debug2.Log("Fire eveywhere !!!", "Particles");
+    }
+}
