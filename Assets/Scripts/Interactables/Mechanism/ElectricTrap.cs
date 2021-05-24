326dd6c722b20a4134fc77589a758869e1f7e23a
Jer D <dambraij@gmail.com>
2021-05-24 10:32:22 +0200

    First electric trap

---
+++ Assets/Scripts/Interactables/Mechanism/ElectricTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricTrap : MonoBehaviour {
+    public float damages = 10;
+    public float lifeTime = 5f;
+
+    private bool active = false;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+   public void Enable() {
+        active = true;
+    }
+
+    public void Disable() {
+        active = false;
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (!active)
+            return;
+
+        if (other.HasComponent(out GenericHealth health)) {
+            health.TakeDamage(damages, this.gameObject);
+        }
+    }
+}
