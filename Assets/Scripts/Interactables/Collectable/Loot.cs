deced0e57f77677f49359007d857e49fe2385158
tibsko <thibaultdion@yahoo.com>
2021-06-20 19:55:25 +0200

    Shader for interactable

--- Assets/Scripts/Interactables/Collectable/Loot.cs
+++ Assets/Scripts/Interactables/Collectable/Loot.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Loot : MonoBehaviour {
 
     [SerializeField] LootRate[] Loots;
+    [SerializeField] GameObject coin;
 
     public void InctantiateLoot() {
 
         int rand = Random.Range(0, 100);
         int total = 0;
+        Instantiate(coin,
+                    transform.position + new Vector3(Random.Range(0f, 1f),
+                    0,
+                    Random.Range(0f, 1f)),
+                    Quaternion.identity);
         for (int i = 0; i < Loots.Length; i++) {
             total += Loots[i].rate;
             if (rand < total) {
